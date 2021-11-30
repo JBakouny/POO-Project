7f27235d668f3283f9287b6710783edc770a1c29
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-11-30 10:38:28 +0200

    Initial Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QString>
+#include <string>
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_goBtn_clicked()
+{
+    QString s = ui->userInput->text();
+    ui->displayLbl->setText(s);
+}
